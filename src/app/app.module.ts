a3c67e1f5ceeb9ca8f205de73ca2e246e1413ba6
unknown <nnja5@o2.pl>
2019-02-02 15:33:16 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListCreaterComponent } from './list-creater/list-creater.component';
+import { ListCreatorComponent } from './list-creator/list-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCreaterComponent,
+    ListCreatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/list-creator/list-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-creator',
+  templateUrl: './list-creator.component.html',
+  styleUrls: ['./list-creator.component.css']
+})
+export class ListCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
